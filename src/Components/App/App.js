f7a4f63a8305054d35042797939d2999999af280
sergiuR <sergiuRemes@users.noreply.github.com>
2022-05-16 11:26:56 +0100

    building static app components

---
+++ src/Components/App/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+// import TrackList from '../TrackList/TrackList';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div class="App">
+          <SearchBar /> 
+          <div className="App-playlist">
+             <SearchResults />
+             <Playlist />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;

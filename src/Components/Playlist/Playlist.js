e0b542c948a00b36ad14044f8bdf7579ada78f55
sergiuR <sergiuRemes@users.noreply.github.com>
2022-05-16 14:45:30 +0100

    local app working, not yet connected to API

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,28 @@
 import React from "react";
 import './Playlist.css';
 
+import Tracklist from "../TrackList/TrackList";
+
 class Playlist extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.handlenameChange = this.handlenameChange.bind(this);
+    }
+
+    handlenameChange(event){
+        this.props.onNameChange(event.target.value);
+
+    }
+
     render() {
         return (
             <div className="Playlist">
-            <input defaultValue={"New Playlist"}/>
-            {/* <Tracklist /> */}
-            <button className="Playlist-save">SAVE TO SPOTIFY</button>
+            <input defaultValue={"New Playlist"} onChange={this.handlenameChange} />
+            <Tracklist tracks={this.props.playlistTracks}
+                        onRemove={this.props.onRemove}
+                        isRemoval={true} /> 
+            <button className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</button>
             </div>
         )
     }
